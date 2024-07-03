bcd0299ea7bb837f1ab7ec9b19346caf1a0a907d
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-07-03 19:06:06 -0300

    :sparkles: feat: adicionando PopUp

---
+++ scripts/pop-up.js
@@ -0,0 +1,9 @@
+const popUp = document.querySelector('.containerPopUp');
+
+function abrirPopUp() {
+    popUp.classList.add('ativo');
+}
+
+function fecharPopUp() {
+    popUp.classList.remove('ativo');
+}
