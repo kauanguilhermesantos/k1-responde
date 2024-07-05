dd78546213afa1f8deebe734f0bf4339b491e92b
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-07-05 19:08:13 -0300

    :recycle: refactor: melhorando o pop up
    ALTERAÇÕES:
    - Substituição do modelo antigo de pop up para o modelo <dialog>.
    
    ADIÇÃO:
    - Estilização do pop up;
    - Maneiras de fechar o pop up (clicando fora do modal, apertando a tecla "esc" e clicando no botão "OK");
    - Animação de abertura

--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -1,9 +1,46 @@
-const popUp = document.querySelector('.containerPopUp');
+// const popUp = document.querySelector('.containerPopUp');
 
-function abrirPopUp() {
-    popUp.classList.add('ativo');
+// function abrirPopUp() {
+//     popUp.classList.add('ativo');
+// }
+
+// function fecharPopUp() {
+//     popUp.classList.remove('ativo');
+// }
+
+const popUp = document.querySelectorAll('.cardQuestao');
+const modal = document.querySelector(".modal");
+const botaoOK = document.querySelector(".botaoOK");
+
+// popUp.onclick = function() {
+//     modal.showModal();
+// }
+
+// botaoOK.onclick = function() {
+//     modal.close();
+// }
+const fecharPopUp = document.querySelector('.containerPopUp');
+
+popUp.forEach(card => {
+  card.addEventListener('click', function() {
+    modal.showModal();
+  });
+});
+
+botaoOK.onclick = function() {
+    fecharPopUp.classList.add('fechar'); // adicionar class pra animação
+    modal.close();
 }
 
-function fecharPopUp() {
-    popUp.classList.remove('ativo');
-}
+modal.addEventListener('click', function(evento) {
+    if (evento.target === modal) {
+      modal.close();
+    }
+});
+
+
+
+// function fecharPopUp() {
+//     if()
+//     fecharPopUp.classList.add('fechar');
+// }
